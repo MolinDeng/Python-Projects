2d1f1a38e8d12b7df41c9e28366b38e8abac9bb1
morning <zjumorning@gmail.com>
2016-11-07 20:58:45 +0800

    simplified

--- strtofloat.py
+++ strtofloat.py
@@ -7,7 +7,6 @@ def char2num(key):
 	return {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9}[key]
 
 def str2float(s1):
-	s2 = s1[:s1.find('.')]+s1[s1.find('.')+1:]
-	return reduce(f,map(char2num,s2))/(10**(len(s1)-1-s1.find('.')))
+	return reduce(f,map(char2num,s1[:s1.find('.')]+s1[s1.find('.')+1:]))/(10**(len(s1)-1-s1.find('.')))
 
 print(str2float(s))
